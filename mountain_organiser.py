6d3832236aa837c809c327bb24c952f3685ff880
AbdullaE100 <Abdullaehsan2002@gmail.com>
2023-05-03 16:21:11 +0800

    completed mountain_organiser.py
    passed test cases completed task

--- mountain_organiser.py
+++ mountain_organiser.py
@@ -1,14 +1,19 @@
 from __future__ import annotations
-
 from mountain import Mountain
+import bisect
 
 class MountainOrganiser:
 
     def __init__(self) -> None:
-        raise NotImplementedError()
-
-    def cur_position(self, mountain: Mountain) -> int:
-        raise NotImplementedError()
+        self.mountains = []
 
     def add_mountains(self, mountains: list[Mountain]) -> None:
-        raise NotImplementedError()
+        for mountain in mountains:
+            bisect.insort(self.mountains, (mountain.length, mountain.name))
+
+    def cur_position(self, mountain: Mountain) -> int:
+        position = bisect.bisect_left(self.mountains, (mountain.length, mountain.name))
+        if position != len(self.mountains) and self.mountains[position] == (mountain.length, mountain.name):
+            return position
+        else:
+            raise KeyError("Mountain not found")
